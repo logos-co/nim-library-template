d69082b55f3645a7cfec6054045aef769db99c7e
Gabriel mermelstein <gabrielmermelstein@gmail.com>
2025-06-18 12:46:55 +0200

    fix: dealloc segfault

--- library/alloc.nim
+++ library/alloc.nim
@@ -43,7 +43,7 @@ proc deallocSharedSeq*[T](s: var SharedSeq[T]) =
           # Deallocate each cstring
           deallocShared(s.data[i])
 
-  deallocShared(s.data)
+    deallocShared(s.data)
   s.len = 0
 
 proc toSeq*[T](s: SharedSeq[T]): seq[T] =
